7a29f3d8814d31410601bcf1a25ba7b1cfd52ee9
Gwen Gibbes <ggibbes@ufl.edu>
2024-04-22 18:42:25 -0400

    Final

---
+++ test/convertUnits.test.js
@@ -0,0 +1,36 @@
+const { expect } = require('chai')
+const {
+  getPounds,
+  getFeetAndInches
+} = require('../util/convertUnits')
+
+describe('unit conversion helpers', () => {
+  describe('getPounds', () => {
+    // Weight of Snorlax
+    it('should convert 4600 hectograms to "1014 pounds"', async () => {
+      expect(getPounds(4600)).to.equal("1014 pounds")
+    })
+    // Weight of Gengar
+    it('should convert 400 hectograms to "88 pounds"', async () => {
+      expect(getPounds(400)).to.equal("88 pounds")
+    })
+    // Weight of Scyther
+    it('should convert 560 hectograms to "123 pounds"', async () => {
+      expect(getPounds(560)).to.equal("123 pounds")
+    })
+  })
+  describe('getFeetAndInches', () => {
+    // Height of Gengar
+    it('should convert 15 decimeters to "4 feet 11 inches"', async () => {
+      expect(getFeetAndInches(15)).to.equal("4 feet 11 inches")
+    })
+    // Height of Snorlax
+    it('should convert 21 decimeters to "6 feet 10 inches"', async () => {
+      expect(getFeetAndInches(21)).to.equal("6 feet 10 inches")
+    })
+    // Height of Gyarados
+    it('should convert 65 decimeters to "21 feet 3 inches"', async () => {
+      expect(getFeetAndInches(65)).to.equal("21 feet 3 inches")
+    })
+  })
+})
